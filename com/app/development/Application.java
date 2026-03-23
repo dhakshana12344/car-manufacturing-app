cf96b52ea64c11f888dc13bb5909416095c669ca
Kumaresan <kkumaresan062@gmail.com>
2026-03-23 22:06:13 +0530

    checkout Branch A

--- com/app/development/Application.java
+++ com/app/development/Application.java
@@ -1,6 +1,30 @@
 package com.app.development;
 public class Application{
 
+	
+	public void oldApplication() {
+		
+		System.out.println("create recover the old ediction");
+	}
+	public void updateApplication() {
+		System.out.println("update Application");
+	}
+	public void currentApplication() {
+		System.out.println("current");
+			
+		
+	}
+	public void employeeSalare() {
+		int sal=200000;
+	}
+	public void meth1() {
+		
+	}
+	public void meth2() {
+		
+	}
+
+
     public static void main(String[] args){
 
         System.out.println("Hello world update git checking !!!");
