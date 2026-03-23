0445731d97868275be27f70bb8bb3eaea7eda2ce
Kumaresan <kkumaresan062@gmail.com>
2026-03-23 14:51:05 +0530

    sumall changes

--- com/app/development/Employee.java
+++ com/app/development/Employee.java
@@ -1,5 +1,16 @@
 package com.app.development;
 
 public class Employee {
-
+	
+public void eSalarey() {
+	int a = 20;
+	int b = 30;
+	int c = a+b;
+	System.out.println(c);
+}
+public static void main(String[] args) {
+	new Employee().eSalarey();
+	System.out.println(123);
+	
+}
 }
