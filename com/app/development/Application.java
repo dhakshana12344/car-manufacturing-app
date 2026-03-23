67082b1e21f2ce170729a599fe05de4290c38773
Kumaresan <kkumaresan062@gmail.com>
2026-03-23 12:57:01 +0530

    #1234: create the new class

--- com/app/development/Application.java
+++ com/app/development/Application.java
@@ -6,5 +6,9 @@ public class Application{
         System.out.println("Hello world update git checking !!!");
         System.out.println("be happy");
         int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+        
     }
 }
