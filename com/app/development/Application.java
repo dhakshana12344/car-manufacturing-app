99d73f41aa746b319104bad8e95d50da005a228c
Kumaresan <kkumaresan062@gmail.com>
2026-03-23 11:39:23 +0530

    add sum values

--- com/app/development/Application.java
+++ com/app/development/Application.java
@@ -4,5 +4,7 @@ public class Application{
     public static void main(String[] args){
 
         System.out.println("Hello world update git checking !!!");
+        System.out.println("be happy");
+        int a=10;
     }
 }
